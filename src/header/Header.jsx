69b21c229dac4fa9c6709e14f9b0732be773be3b
charafi <progpt.co@gmail.com>
2026-06-12 21:17:11 +0200

    responsive mobile app css 2

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 
 export default function Header() {
-  const { user, logout } = useAuth()  // ← ajoute user
+  const { user, logout } = useAuth()
   const navigate = useNavigate()
 
   function handleLogout() {
@@ -13,13 +13,28 @@ export default function Header() {
   return (
     <header className="nav">
       <nav>
+        {user && (
+            <button onClick={handleLogout} title="Déconnexion" style={{
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+              padding: "0",
+              lineHeight: "1",
+              color: "#444"
+            }}>
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#444" strokeWidth="2">
+              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/>
+              <polyline points="16 17 21 12 16 7"/>
+              <line x1="21" y1="12" x2="9" y2="12"/>
+            </svg>
+          </button>
+        )}
         <Link to="/factures" style={{ fontWeight: 600, fontSize: 16 }}>Facturation</Link>
         {user && (
           <>
-            <Link to="/clients" style={{ fontWeight: 600, fontSize: 16 }} >Clients</Link>
-            <Link to="/products" style={{ fontWeight: 600, fontSize: 16 }}>Produits</Link>
-            <Link to="/orders" style={{ fontWeight: 600, fontSize: 16 }}>Commandes</Link>
-            <button onClick={handleLogout}>Déconnexion</button>
+            <Link to="/clients">Clients</Link>
+            <Link to="/products">Produits</Link>
+            <Link to="/orders">Commandes</Link>
           </>
         )}
       </nav>
